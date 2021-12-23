32dc6df5fb024b172146a93a0f37acad5ab29574
Elektra <allanaleerskov@gmail.com>
2021-12-23 15:39:15 -0700

    generate index.html

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const profileDataArgs = process.argv.slice(2);
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-    //this...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
-    console.log('================');
-    //is the same as this
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const generatePage = require('./src/page-template');
 
-printProfileData(profileDataArgs);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw new Error (err);
+
+    console.log('Portfolio complete! Check out index.html')
+});
