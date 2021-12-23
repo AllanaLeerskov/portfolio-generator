b7a53fb0d534e8acd3bcc36a3330b681c2fad575
Elektra <allanaleerskov@gmail.com>
2021-12-23 14:24:00 -0700

    deleted console log 2

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
 
 const printProfileData = profileDataArr => {
     //this...
-    for (let i = 0; i < profileDataArr.length; i +=1) {
+    for (let i = 0; i < profileDataArr.length; i += 1) {
         console.log(profileDataArr[i]);
     }
-    console.log('=============');
+    console.log('================');
     //is the same as this
     profileDataArr.forEach(profileItem => console.log(profileItem));
-    
 };
+
 printProfileData(profileDataArgs);
